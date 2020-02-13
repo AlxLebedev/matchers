112734e2cba612b029917ec1bf02c335aff3ada0
Alexey <vorresti@ya.ru>
2020-02-13 15:39:45 +0300

    Index.html modified -- testing AppVeour

--- src/js/__tests__/sortArray.test.js
+++ src/js/__tests__/sortArray.test.js
@@ -3,16 +3,10 @@ import sortArray from '../sortArray';
 import characters from '../data';
 
 test('Array must be sortered from max to min', () => {
-  // const input = [
-  //   {name: 'мечник', health: 10},
-  //   {name: 'маг', health: 100},
-  //   {name: 'лучник', health: 80},
-  // ];
-
   const expected = [
-    {name: 'маг', health: 100},
-    {name: 'лучник', health: 80},
-    {name: 'мечник', health: 10},
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ];
 
   const recieved = sortArray(characters);
