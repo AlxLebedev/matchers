ce56b111bbed88f89293b6b934332516b1a38631
Alexey Lebedev <vorresti@ya.ru>
2020-02-16 19:56:24 +0300

    tests modified

--- src/js/__tests__/sortArray.test.js
+++ src/js/__tests__/sortArray.test.js
@@ -2,14 +2,29 @@ import sortArray from '../sortArray';
 
 import characters from '../data';
 
-test('Array must be sortered from max to min', () => {
-  const expected = [
-    { name: 'маг', health: 100 },
-    { name: 'лучник', health: 80 },
-    { name: 'мечник', health: 10 },
-  ];
+// test('Array must be sortered from max to min', () => {
+//   const expected = [
+//     { name: 'маг', health: 100 },
+//     { name: 'лучник', health: 80 },
+//     { name: 'мечник', health: 10 },
+//   ];
 
+//   const recieved = sortArray(characters);
+
+//   expect(recieved).toEqual(expected);
+// });
+
+test('Array must be sortered from max to min', () => {
   const recieved = sortArray(characters);
 
-  expect(recieved).toEqual(expected);
+  function isSorted(arr) {
+    for (let i = 0; i < arr.length - 1; i += 1) {
+      if (arr[i].health < arr[i + 1].health) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  expect(true).toEqual(isSorted(recieved));
 });
